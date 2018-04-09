f912b01d7b07b09d7e1a7d23783e5477db06f1dc
Martijn Dortmond <martijndortmond@hotmail.com>
2018-04-09 12:35:44 +0200

    pandas

--- reader.py
+++ reader.py
@@ -1,54 +1,52 @@
-import csv
-
-with open('ODI-2018.csv') as data:
-    reader = csv.reader(data, delimiter = ",")
-
-    data = (list(reader)[2:])
-
-    def time(data):
-        pass
-
-    def programme(data):
-        pass
-
-    def information_retreaval(data):
-        pass
-
-    def machine_learning(data):
-        pass
-
-    def statistics_course(data):
-        pass
-
-    def database_course(data):
-        pass
-
-    def gender(data):
-        pass
-
-    def chocolate(data):
-        pass
-
-    def neighbors(data):
-        pass
-
-    def standup(data):
-        pass
-
-    def money(data):
-        pass
-
-    def random_num(data):
-        pass
-
-    def bedtime(data):
-        pass
-
-    def goodday(data1, data2):
-        pass
+import pandas as pd
 
 
+def time(data):
+    pass
 
-    #Standard info
-    print("Amount of records: ", len(data))
-    print("Amount of attributes:", len(data[0]))
+def programme(data):
+    pass
+
+def information_retreaval(data):
+    pass
+
+def machine_learning(data):
+    pass
+
+def statistics_course(data):
+    pass
+
+def database_course(data):
+    pass
+
+def gender(data):
+    pass
+
+def chocolate(data):
+    pass
+
+def birthday(data):
+    pass
+
+def neighbors(data):
+    pass
+
+def standup(data):
+    pass
+
+def money(data):
+    pass
+
+def random_num(data):
+    pass
+
+def bedtime(data):
+    pass
+
+def goodday(data1, data2):
+    pass
+
+headers = ["time", "programme", "machine_learning", "information_retreaval",
+"statistics_course", "database_course", "gender", "chocolate", "birthday", "neighbors", "standup", "money", "random_num", "bedtime", "goodday1", "goodday2" ]
+data = pd.read_csv('ODI-2018.csv', delimiter = ',', names = headers, skiprows = 2)
+print (data)
