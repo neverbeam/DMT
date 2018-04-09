0764cd89cbcae3fbe721a07ba2e328e539c0a469
Martijn Dortmond <martijndortmond@hotmail.com>
2018-04-09 12:01:26 +0200

    update reader with function

--- reader.py
+++ reader.py
@@ -3,15 +3,52 @@ import csv
 with open('ODI-2018.csv') as data:
     reader = csv.reader(data, delimiter = ",")
 
-    # Skip the header lines
-    for i in range(2):
-        next(reader, None)
+    data = (list(reader)[2:])
 
-    tot = 0
-    for row in reader:
-        tot += 1
-        print (row)
+    def time(data):
+        pass
+
+    def programme(data):
+        pass
+
+    def information_retreaval(data):
+        pass
+
+    def machine_learning(data):
+        pass
+
+    def statistics_course(data):
+        pass
+
+    def database_course(data):
+        pass
+
+    def gender(data):
+        pass
+
+    def chocolate(data):
+        pass
+
+    def neighbors(data):
+        pass
+
+    def standup(data):
+        pass
+
+    def money(data):
+        pass
+
+    def random_num(data):
+        pass
+
+    def bedtime(data):
+        pass
+
+    def goodday(data1, data2):
+        pass
+
+
 
     #Standard info
-    print("Amount of records: ", tot)
-    print("Amount of attributes:", len(row))
+    print("Amount of records: ", len(data))
+    print("Amount of attributes:", len(data[0]))
