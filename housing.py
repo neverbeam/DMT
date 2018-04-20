eaad0020ad6f6fc7ddcb6ce813f792800c683477
neverbeam <martijndortmond@hotmail.com>
2018-04-20 14:01:43 +0200

    updated housing
    yeah housing

--- housing.py
+++ housing.py
@@ -1,17 +1,7 @@
 import pandas as pd
-from sklearn import linear_model
+from sklearn import linear_model,svm
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-
-def location(data):
-    """Parse location indexes to an interger ranking scheme. """
-    if data == "A":
-        return 1
-    elif data == "C":
-        return 2
-    elif data == "FV":
-        return 3
-    else:
-        return 4
+from sklearn.preprocessing import PolynomialFeatures
 
 def split_data(data, p=0.5):
     """Split the data into a learn and test set."""
@@ -26,46 +16,41 @@ def learn_linear_regression(data):
     """ Fit a linear regression through the datapoints."""
     regression = linear_model.LinearRegression()
 
-    data_train = data[["LotArea", "YearBuilt", "MSSubClass", "OverallCond", "MSZoning"]]
+    data_train = data[["LotArea", "YearBuilt", "MSSubClass", "OverallCond","BedroomAbvGr","TotRmsAbvGrd", "FullBath","BsmtFullBath", "GarageArea", "OverallQual", "TotalBsmtSF", "YrSold"]]
     label_train = data["SalePrice"]
 
     regression.fit(data_train, label_train)
     
     return regression
 
-def learn_polynomial(data):
+def learn_SVR(data):
+    """ Fit a support vector regression through the datapoints."""
+    regression = svm.SVR()
 
-    from sklearn.preprocessing import PolynomialFeatures
-    from sklearn.linear_model import LinearRegression
-    from sklearn.pipeline import Pipeline
-    import numpy as np
-    model = Pipeline([('poly', PolynomialFeatures(degree=3)),
-                          ('linear', LinearRegression(fit_intercept=False))])
-    # fit to an order-3 polynomial data
-    x = np.arange(5)
-    y = 3 - 2 * x + x ** 2 - x ** 3
-    model = model.fit(x[:, np.newaxis], y)
-    model.named_steps['linear'].coef_
+    data_train = data[["LotArea", "YearBuilt", "MSSubClass", "OverallCond","BedroomAbvGr","TotRmsAbvGrd", "FullBath","BsmtFullBath", "GarageArea", "OverallQual", "TotalBsmtSF", "YrSold"]]
+    label_train = data["SalePrice"]
 
+    regression.fit(data_train, label_train)
+    
+    return regression
 
 if __name__ == '__main__':
     
     # Read the data for housing prices.
     data = pd.read_csv('housing_data.csv', delimiter = ',') 
-    data["MSZoning"] = data["MSZoning"].apply(location)
     learn_data, test_data = split_data(data)
     
     # Create the linear regressor and predict.
     regressor = learn_linear_regression(learn_data)
-    prediction = regressor.predict(test_data[["LotArea", "YearBuilt", "MSSubClass", "OverallCond", "MSZoning"]])
-    
-    #   
+    prediction = regressor.predict(test_data[["LotArea", "YearBuilt", "MSSubClass", "OverallCond", "BedroomAbvGr","TotRmsAbvGrd","FullBath", "BsmtFullBath", "GarageArea", "OverallQual", "TotalBsmtSF", "YrSold"]])
+   
     print(mean_squared_error(prediction, test_data["SalePrice"]))
     print(mean_absolute_error(prediction, test_data["SalePrice"]))
     
-    # Create the polynomial regressor and predict.
-    regressor = learn_polynomial(learn_data)
-    prediction = regressor.predict(test_data[["LotArea", "YearBuilt", "MSSubClass", "OverallCond", "MSZoning"]])
+    #Create the Support vector regressor and predict.
+    regressor = learn_SVR(learn_data)
+    prediction = regressor.predict(test_data[["LotArea", "YearBuilt", "MSSubClass", "OverallCond", "BedroomAbvGr","TotRmsAbvGrd","FullBath", "BsmtFullBath", "GarageArea", "OverallQual", "TotalBsmtSF", "YrSold"]])
         
     print(mean_squared_error(prediction, test_data["SalePrice"]))
     print(mean_absolute_error(prediction, test_data["SalePrice"]))
+    
