182f8ac2d931c13a8c8e02976ceb1005f42d0d93
Patrick Wernke <patrick@bit-students.com>
2018-05-05 00:35:32 +0200

    testen is soort van mogelijk

--- Ass2/reader.py
+++ Ass2/reader.py
@@ -21,7 +21,7 @@ def parse_data(data, categories, show=False):
             column_to_pie(data, category)
 
     data["date_time"] = data["date_time"].apply(parse_date_time)
-    
+
     return data
 
 def stripcompetition(item):
@@ -30,16 +30,57 @@ def stripcompetition(item):
     else:
         return item
     
-
 def changecompetition(data, categories):
     for category in categories[-24:-3]:
         data[category] = data[category].apply(stripcompetition)
         
     return data
 
+# split the data in a learn and test set based on search ids
+def split_data(data, p=0.5):
+    """Split the data into a learn and test set."""
+    # get all different search ids and shuffle them
+    srch_ids = data["srch_id"].unique()
+    np.random.shuffle(srch_ids)
+
+    # split the ids into a learn and test set
+    split_at = int(p*len(srch_ids))
+    learn = data.loc[data['srch_id'].isin(srch_ids[:split_at])]
+    test = data.loc[data['srch_id'].isin(srch_ids[split_at:])]
+
+    return learn, test
+
+# returns rows where the search id is the one given
+def get_single_query(data, srch_id):
+    return data.loc[data['srch_id'] == srch_id]
+
+# returns the rows without bookings of a randomly picked query_id
+def get_single_test(test_data):
+    srch_id = test_data.sample().iloc[0]['srch_id'] #just get that 1 value
+    query_with_booking = get_single_query(test_data, srch_id)
+    query_test = query_with_booking.drop(columns=["click_bool","gross_bookings_usd","booking_bool"])
+    return query_test, srch_id
+
+# given a prediction ordering of likeliness that hotel was booked,
+# returns a value describing how good the prediction is
+# voor nu nog: pakt de top 5 en kijkt of die geboekt zijn en geeft er punten voor
+# later moet dit nog worden gedaan met cummulative gain etc
+def try_single_test(test_data, srch_id, predictions):
+    query_with_booking = get_single_query(test_data, srch_id)
+    score = 0
+    for i in range(5):
+        prediction = predictions[i]
+        predicted_row = query_with_booking.loc[query_with_booking['prop_id'] == prediction]
+        if predicted_row.iloc[0]['click_bool'] == 1:
+            if predicted_row.iloc[0]['booking_bool'] == 1:
+                score += 5
+            else:
+                score += 1
+
+    return score
+
 if __name__ == '__main__':
     train_data = pd.read_csv('training_set_VU_DM_2014_small.csv', delimiter = ',')
-    test_data = pd.read_csv('test_set_VU_DM_2014_small.csv', delimiter = ',')
 
     # all column headers
     categories = list(train_data)
@@ -47,5 +88,29 @@ if __name__ == '__main__':
     train_data = parse_data(train_data, categories)
 
     train_data = changecompetition(train_data, categories)
-    print (train_data["comp2_rate"])
+    # print (train_data["comp2_rate"])
     
+    # correlations = train_data[2:23].corr()
+    # # look at the query range
+    # for i in range(2,24):
+    #     print("\n===============" + categories[i])
+    #     print(train_data[2:23].corr()[categories[i]])
+
+
+
+    # LEARNING, PREDICTING, SCORING
+    # split the data
+    learn_data, test_data = split_data(train_data)
+
+    # do some learn step here
+    
+    # get a single test query
+    query_test, srch_id = get_single_test(test_data)
+
+    # predict the order of the prop_id (for now hardcoded list of numbers to test)
+    predictions = list(query_test["prop_id"]) # <- pls order this
+
+    # test your prediction
+    result = try_single_test(test_data, srch_id, predictions)
+
+    print(result)
